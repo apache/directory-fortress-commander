2f1eeeb7c7351ebb977f1e04206504a62dda027a
Shawn McKinney <shawn.mckinney@jts.us>
2013-11-11 13:29:00 -0600

    COM-84 - RC33 Release

--- src/test/java/us/jts/commander/StartExamples.java
+++ src/test/java/us/jts/commander/StartExamples.java
@@ -33,7 +33,7 @@ public class StartExamples
 		Server server = new Server();
 		SocketConnector connector = new SocketConnector();
 
-        System.setProperty("version", "1.0-RC27");
+        System.setProperty("version", "1.0-RC33");
 
 		// Set some timeout options to make debugging easier.
 		connector.setMaxIdleTime(1000 * 60 * 60);
